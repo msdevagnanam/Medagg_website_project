bb347b28083ac35438e750c0efdac1136901fa6f
msdevagnanam <114239887+msdevagnanam@users.noreply.github.com>
2025-07-09 21:20:16 +0530

    fix: set base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Medagg_website_project',  // 👈 must match your GitHub repo name
+  base: '/Medagg_website_project/',  // 👈 must match your GitHub repo name
   plugins: [react()]
 })
